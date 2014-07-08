0b19e03f1e51dc09dffd8b816c59f93c3e487fac
Fatima <faa@yorku.ca>
2014-07-08 14:56:01 -0400

    my first commit

---
+++ src/setup.py
@@ -0,0 +1,27 @@
+__author__="Fatima"
+__date__ ="$7-Jul-2014 5:51:56 PM$"
+
+from setuptools import setup,find_packages
+
+setup (
+  name = 'UMBC',
+  version = '0.1',
+  packages = find_packages(),
+
+  # Declare your packages' dependencies here, for eg:
+  install_requires=['foo>=3'],
+
+  # Fill in these to make your Egg ready for upload to
+  # PyPI
+  author = 'Fatima',
+  author_email = '',
+
+  summary = 'Just another Python package for the cheese shop',
+  url = '',
+  license = '',
+  long_description= 'Long description of the package',
+
+  # could also include long_description, download_url, classifiers, etc.
+
+  
+)
