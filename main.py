46c048a19855d5826e94e6c5ebbe3d87348d39c8
Fatima <faa@yorku.ca>
2014-08-05 21:28:39 -0400

    Make the link direct without /request + add all the goals for nurse domain

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ import nltk
 import quepy
 from SPARQLWrapper import SPARQLWrapper, JSON
 from umbc2 import sss
+from Nurse import nursedomain
+from NLTK import ie_preprocess
+import string
 
 bottle.debug(True)
 
@@ -33,27 +36,119 @@ def index():
     return
 """
 #   @get("/tryqustion") # or @route('/login')
-@bottle.route("/request")
+@bottle.route("/")
 def tryquestion():
  return '''
-    <form action="/request" method="post">
-        Enter you NLQ: <input name="request" type="text" size="65" />
+    <form action="/" method="post">
+        Enter you NLQ: <input name="query" type="text" size="65" />
         <input value="Search" type="submit" />
      </form>
     '''
 #@post('/tryqustion') # or @route('/login', method='POST')
-@bottle.route("/request", method="POST")
+@bottle.route("/", method="POST")
 def answerq():
-    q = request.forms.get('request')
+    q = request.forms.get('query')
     
-    Goal1 = "Add comment is never satisfied"
-    Goal2 = "View prices is not satisfied before Login is satisfied"
+    Goal1 = "happy nurse"
+    Goal2 = "happy patient"
+    Goal3 = "nurse comfort"
+    Goal4 = "patient feels cared for"
+    Goal5 = "nurse attend to patient"
+    Goal6 = "nurse notified"
+    Goal7 = "system notifies the nurse through speakers"
+    Goal8 = "system notifies the nurse through earphones"
+    Goal9 = "nurse responded call"
+    Goal10 = "nurse talked with patient"
+    #Goal11 = "Nurse doesn't Talk with Patient"
+    Goal12 = "nurse visited patient"
+    Goal13 = "nurse walks to patients room"
+    Goal14 = "nurse skips visit"
+    Goal15 = "nurse talks with patient by mobile"
+    #Goal16 = "Nurse Talks with patient from Nursing Station"
+    Goal17 = "nurse walks to the nurse station"
+    Goal18 = "nurse talks to the patient at the nurse station"
+    Goal19 = "nurse turns request off"
+    Goal20 = "increase nurse productivity"
+    Goal21 = "no patient disturbance"
+    Goal22 = "no nurse disturbance"
     
-    for x in range(0,1):
+    res1 = str(sss(q,Goal1))
+    res2 = str(sss(q,Goal2))
+    res3 = str(sss(q,Goal3))
+    res4 = str(sss(q,Goal4))
+    res5 = str(sss(q,Goal5))
+    res6 = str(sss(q,Goal6))
+    res7 = str(sss(q,Goal7))
+    res8 = str(sss(q,Goal8))
+    res9 = str(sss(q,Goal9))
+    res10 = str(sss(q,Goal10))
+    #res11 = str(sss(q,Goal11))
+    res12 = str(sss(q,Goal12))
+    res13 = str(sss(q,Goal13))
+    res14 = str(sss(q,Goal14))
+    res15 = str(sss(q,Goal15))
+    #res16 = str(sss(q,Goal16))
+    res17 = str(sss(q,Goal17))
+    res18 = str(sss(q,Goal18))
+    res19 = str(sss(q,Goal19))
+    res20 = str(sss(q,Goal20))
+    res21 = str(sss(q,Goal21))
+    res22 = str(sss(q,Goal22))
+    
+    
+    #for x in range(0,1):
     #expression = sss(q,Goal1)
-        yield u"<p>The similarity score between " + q + u"<a> and "u"</a>" + Goal1 + u"<a> is: "u"</a>" + str(sss(q,Goal1)) + u"</p>"
-        yield u"<p>The similarity score between " + q + u"<a> and "u"</a>" + Goal2 + u"<a> is: "u"</a>" + str(sss(q,Goal2)) + u"</p>"
+    
+    yield u"<p>The similarity score between " + q + u"<a> and </a>"u"</p>"
+    yield u"<p> " + Goal1 + u"<a> is: "u"</a>" + res1 + u"</p>"
+    yield u"<p> " + Goal2 + u"<a> is: "u"</a>" + res2 + u"</p>"
+    yield u"<p> " + Goal3 + u"<a> is: "u"</a>" + res3 + u"</p>"
+    yield u"<p> " + Goal4 + u"<a> is: "u"</a>" + res4 + u"</p>"
+    yield u"<p> " + Goal5 + u"<a> is: "u"</a>" + res5 + u"</p>"
+    yield u"<p> " + Goal6 + u"<a> is: "u"</a>" + res6 + u"</p>"
+    yield u"<p> " + Goal7 + u"<a> is: "u"</a>" + res7 + u"</p>"
+    yield u"<p> " + Goal8 + u"<a> is: "u"</a>" + res8 + u"</p>"
+    yield u"<p> " + Goal9 + u"<a> is: "u"</a>" + res9 + u"</p>"
+    yield u"<p> " + Goal10 + u"<a> is: "u"</a>" + res10 + u"</p>"
+    #yield u"<p> " + Goal11 + u"<a> is: "u"</a>" + res11 + u"</p>"
+    yield u"<p> " + Goal12 + u"<a> is: "u"</a>" + res12 + u"</p>"
+    yield u"<p> " + Goal13 + u"<a> is: "u"</a>" + res13 + u"</p>"
+    yield u"<p> " + Goal14 + u"<a> is: "u"</a>" + res14 + u"</p>"
+    yield u"<p> " + Goal15 + u"<a> is: "u"</a>" + res15 + u"</p>"
+    #yield u"<p> " + Goal16 + u"<a> is: "u"</a>" + res16 + u"</p>"
+    yield u"<p> " + Goal17 + u"<a> is: "u"</a>" + res17 + u"</p>"
+    yield u"<p> " + Goal18 + u"<a> is: "u"</a>" + res18 + u"</p>"
+    yield u"<p> " + Goal19 + u"<a> is: "u"</a>" + res19 + u"</p>"
+    yield u"<p> " + Goal20 + u"<a> is: "u"</a>" + res20 + u"</p>"
+    yield u"<p> " + Goal21 + u"<a> is: "u"</a>" + res21 + u"</p>"
+    yield u"<p> " + Goal22 + u"<a> is: "u"</a>" + res22 + u"</p>"
+       
+    yield max(res1, res2, res3, res4, res5, res6, res7, res8, res9, res10, res12, res13, res14,  \
+    res15, res17, res18, res18, res20, res21, res22)
+    
+    # Get a bag of words without punctuation
+    words = [word.strip(string.punctuation) for word in q.split()]
+    print words
+    
+    # Create a list and add the words to the list
+    my_list = [] 
+    for current_word in words:
+        my_list.append(current_word.lower())
         
+    # Create a list with negative words
+    NegativeWords = ["not", "haven't", "havent", "hasn't", "hasnt", "didn't", "didnt", "doesn't",\
+    "doesnt", "don't", "dont", "shouldn't", "shouldnt", "couldn't", "couldnt", "can't", "cant", "cannot"]
+    
+    # Check if the sentence written have any negative words
+    FoundNeg = set(NegativeWords).intersection(my_list)
+    print FoundNeg
+    
+    if FoundNeg:
+        yield u"<p>The sentence is in Negative form</p>"
+
+    
+    #ie_preprocess(q) # test NLTK
+    
    # text = nltk.word_tokenize(q)
    # print nltk.pos_tag(text) 
     #print expression
